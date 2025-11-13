92448c419d5fd2c4289befab06dce0d8692062e3
João Pedro A.P.P. Barbosa <jpandradepaesdev@gmail.com>
2025-11-13 09:09:34 -0300

    UPDATED pom.xml
    Algumas dependencias estavam faltando para fazer o ID funcionar

--- src/main/java/prop/visual/ChatClientVisual.java
+++ src/main/java/prop/visual/ChatClientVisual.java
@@ -4,8 +4,6 @@ import javax.swing.*;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 import javax.jms.*;
 import org.apache.activemq.ActiveMQConnectionFactory;
 
